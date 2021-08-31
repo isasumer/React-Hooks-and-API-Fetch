6ba16fb8b64886bc7fd7a4fef44e1cce32125c73
Isa SUMER <sumerisa5@gmail.com>
2021-08-31 10:48:11 +0300

    classComponent added

---
+++ react-hooks-and-api-fetch/src/ClassComponent.js
@@ -0,0 +1,12 @@
+import React from "react";
+class ClassComponents extends React.Component {
+    render(){
+        return (
+            <div className="class">
+                <h2>Class Component</h2>
+            </div>
+        )
+    }
+}
+
+export default ClassComponents;
