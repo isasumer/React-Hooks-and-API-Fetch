d7705b75e3c41a19169c9ac5d984b0eb7bc1b7ec
Isa SUMER <sumerisa5@gmail.com>
2021-08-31 10:48:21 +0300

    functional component added

---
+++ react-hooks-and-api-fetch/src/FunctionalComponents.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FunctionalComponent = () => {
+    return (
+        <div className="function">
+            <h2>Functional Component</h2>
+        </div>
+    )
+}
+
+export default FunctionalComponent;
