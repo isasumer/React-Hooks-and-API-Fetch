5427d2701812264183b42b8a36071a24aafe1ebf
Isa SUMER <sumerisa5@gmail.com>
2021-08-31 12:06:20 +0300

    button added

--- react-hooks-and-api-fetch/src/ClassComponent.js
+++ react-hooks-and-api-fetch/src/ClassComponent.js
@@ -1,9 +1,27 @@
 import React from "react";
+
 class ClassComponents extends React.Component {
+    // constructor (){
+    //     super();
+    //     this.state = {
+    //         counter:0
+    //     }
+    //     this.increase = this.increase.bind(this);
+    // };
+    state = {
+        counter:0
+    };
+
+increase =() => {
+   this.setState({counter: this.state.counter+1})
+}
+
     render(){
         return (
             <div className="class">
                 <h2>Class Component</h2>
+                <p>Counter: {this.state.counter}</p>
+                <button onClick= {() => this.increase()}>increase</button>
             </div>
         )
     }
--- react-hooks-and-api-fetch/src/FunctionalComponents.js
+++ react-hooks-and-api-fetch/src/FunctionalComponents.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const FunctionalComponent = () => {
+    const [counter, setCounter] = useState(0)
+
+    const increase = () => {
+        setCounter(counter + 1)
+    }
+
     return (
         <div className="function">
             <h2>Functional Component</h2>
+            <p>Counter: {counter}</p>
+            <button onClick= {()=>{increase()}}>increase</button>
         </div>
     )
 }
